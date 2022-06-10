3b3d509da3de6bcaf53c8c259e0f4aaa77f68aa6
Arve Seljebu <arve.seljebu@gmail.com>
2022-06-10 12:47:38 +0200

    init

---
+++ cli.ts
@@ -0,0 +1,27 @@
+import cliTable from "cli-table3";
+import commander from "commander";
+import debug from "debug";
+import nodeFetch from "node-fetch";
+import read from "read";
+import simpleGit from "simple-git";
+import yaml from "yaml";
+import zod from "zod";
+
+const modules = {
+  cliTable: cliTable,
+  commander: commander,
+  debug: debug,
+  nodeFetch: nodeFetch,
+  read: read,
+  simpleGit: simpleGit,
+  yaml: yaml,
+  zod: zod,
+}
+
+for (const [name, module] of Object.entries(modules)) {
+  if (module) {
+    console.log("✅", name)
+  } else {
+    console.log("❌", name)
+  }
+}
